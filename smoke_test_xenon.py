4013f5a8cfc45f0b2aae84c5e380594b94661d44
FlorisOrd <florisordelmans@outlook.com>
2026-03-25 16:47:01 +0100

    Clean up temporary setup files

--- smoke_test_xenon.py
+++
@@ -1,9 +0,0 @@
-from xe_likelihood import BinwiseInference, Spectrum
-
-print("Import worked")
-
-spectrum = Spectrum.from_wimp(mass=50)
-print("Spectrum created:", type(spectrum).__name__)
-
-inference = BinwiseInference.from_xenon1t_sr(spectrum=spectrum)
-print("Inference created:", type(inference).__name__)
